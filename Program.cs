96dcda26d83e5cfe3e6d0975f9fa93e1fe16541a
thijmen <PUBGtjes@gmail.com>
2022-10-04 15:07:52 +0200

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿
+namespace Bony
+{
+
+    class main
+    {
+
+        static void Main(string[] args)
+        {
+
+            string path = args[0];
+            string[] paths = path.Split('/');
+            String pathstring = "";
+            int amount = paths[paths.Length - 1].Contains('.') ? 1 : 0;
+            for (var i = 0; i < paths.Length - amount; i++)
+            {
+                
+            
+                pathstring += paths[i];
+                if (!Directory.Exists(pathstring))
+                {
+                    Directory.CreateDirectory(pathstring);
+                }
+
+                pathstring += "/";
+
+
+
+            }
+            if (paths[paths.Length - 1].Contains('.'))
+            {
+                File.Create(pathstring + "/" + paths[^1], 0);
+            }
+            
+            Console.WriteLine(paths[paths.Length - 1].Contains('.') ? "Directory created successfully" : "Directory + Files created successfully!");
+        }
+    }
+}
