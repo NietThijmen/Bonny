d2ae11f53ac08ea61adc73b5e1a669b557bd6300
thijmen <PUBGtjes@gmail.com>
2022-10-04 15:20:40 +0200

    Added checks for empty stuff!

--- Program.cs
+++ Program.cs
@@ -2,37 +2,55 @@
 namespace Bony
 {
 
-    class main
+    class main 
     {
 
         static void Main(string[] args)
         {
-
-            string path = args[0];
-            string[] paths = path.Split('/');
-            String pathstring = "";
-            int amount = paths[paths.Length - 1].Contains('.') ? 1 : 0;
-            for (var i = 0; i < paths.Length - amount; i++)
+            if (args.Length == 0)
             {
-                
-            
-                pathstring += paths[i];
-                if (!Directory.Exists(pathstring))
+                Console.WriteLine("You need to specify a dir (+ file)");
+
+            }
+            else
+            {
+
+
+                string path = args[0];
+                if (string.IsNullOrEmpty(path))
                 {
-                    Directory.CreateDirectory(pathstring);
+                    Console.WriteLine("You need to specify a dir (+ file)");
                 }
+                else
+                {
 
-                pathstring += "/";
+
+                    string[] paths = path.Split('/');
+                    String pathstring = "";
+                    int amount = paths[paths.Length - 1].Contains('.') ? 1 : 0;
+                    for (var i = 0; i < paths.Length - amount; i++)
+                    {
+
+
+                        pathstring += paths[i];
+                        if (!Directory.Exists(pathstring))
+                        {
+                            Directory.CreateDirectory(pathstring);
+                        }
+
+                        pathstring += "/";
 
 
 
+                    }
+                    if (paths[paths.Length - 1].Contains('.'))
+                    {
+                        File.Create(pathstring + "/" + paths[^1], 0);
+                    }
+
+                    Console.WriteLine($"Directory (+ Files) created successfully! {pathstring}");
+                }
             }
-            if (paths[paths.Length - 1].Contains('.'))
-            {
-                File.Create(pathstring + "/" + paths[^1], 0);
-            }
-            
-            Console.WriteLine(paths[paths.Length - 1].Contains('.') ? "Directory created successfully" : "Directory + Files created successfully!");
         }
     }
 }
