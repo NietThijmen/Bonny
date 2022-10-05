493d20775554c4e711309531a3a0fe158a8d4c56
thijmen <PUBGtjes@gmail.com>
2022-10-05 10:55:04 +0200

    Refactoring/Global imports
    Changed some array stuff. and did global imports. gotta go FAST

--- Program.cs
+++ Program.cs
@@ -1,26 +1,30 @@
-﻿namespace Bony
+﻿using static System.Console;
+using static System.IO.Directory;
+using static System.IO.File;
+
+namespace Bony
 {
     class main
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
             if (args.Length == 0)
             {
-                Console.WriteLine("Please add a directory (+file)");
+                WriteLine("Please add a directory (+file)");
                 return;
             }
             string path = args[0];
             string[] paths = path.Split("/");
-            int IsOnlyFileOrFileWithADiir = 0;
+            int isOnlyFileOrFileWithADir = 0;
             if (paths[^1].Contains('.'))
             {
-                IsOnlyFileOrFileWithADiir = 1;
+                isOnlyFileOrFileWithADir = 1;
             }
             if (paths.Length > 1)
             {
                 String pathstring = "";
 
-                for (int index = 0; index < paths.Length - IsOnlyFileOrFileWithADiir; index++)
+                for (int index = 0; index < paths.Length - isOnlyFileOrFileWithADir; index++)
                 {
                     string t = paths[index];
                     pathstring += t;
@@ -28,27 +32,27 @@
                     {
                         if (!Directory.Exists(pathstring))
                         {
-                            Directory.CreateDirectory(pathstring);
+                            CreateDirectory(pathstring);
                         }
                     }
                     catch (Exception)
                     {
-                        Console.WriteLine("Path cannot begin by / or ./");
-                        Console.WriteLine("Correct usage is path/");
-                        Console.WriteLine("Not /path/");
+                        WriteLine("Path cannot begin by / or ./");
+                        WriteLine("Correct usage is path/");
+                        WriteLine("Not /path/");
                     }
 
                     pathstring += "/";
                 }
 
-                if (IsOnlyFileOrFileWithADiir == 1)
+                if (isOnlyFileOrFileWithADir == 1)
                 {
-                    File.Create($"{pathstring}{paths[paths.Length - 1]}", 0);
+                    Create($"{pathstring}{paths[^1]}", 0);
                 }
             }
             else
             {
-                File.Create($"{Directory.GetCurrentDirectory()}/{paths[paths.Length - 1]}", 0);
+                Create($"{GetCurrentDirectory()}/{paths[^1]}", 0);
             }
         }
     }
